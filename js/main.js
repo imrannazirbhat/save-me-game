5df03f1199f26be40a4a267d0333497e9612b0a9
ImraN <imran.bhat@hotmail.com>
2019-06-09 15:00:02 +0530

    Added navigation and obstacles

--- js/main.js
+++ js/main.js
@@ -1,36 +1,103 @@
 var canvas = document.querySelector('canvas');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.width = 600;//window.innerWidth;
+canvas.height = 450;//window.innerHeight;
 
 var context = canvas.getContext("2d");
 
 
-var x = 200;
-var dx = 2;
-var y = 200;
-var dy = 2;
 
-var radius = 30;
+function Circle(x, y, dx, dy, radius, who) {
+
+  this.x = x;
+  this.y = y;
+  this.dx = dx;
+  this.dy = dy;
+  this.radius= radius;
+  this.who = who;
+
+  this.draw = function() {
+
+    context.beginPath();
+    context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    context.strokeStyle = (this.who == 'saveMe' ? 'green' : 'red');
+    context.stroke();
+    context.fillStyle = (this.who == 'saveMe' ? 'green' : 'red');
+    context.fill();
+  }
+
+  this.update = function() {
+
+    if(this.who != 'saveMe') {
+
+      if(this.x + this.radius > canvas.width || this.x - this.radius < 0) {
+        this.dx = -this.dx;
+      }
+
+      if(this.y + this.radius > canvas.height || this.y - this.radius < 0) {
+        this.dy = -this.dy;
+      }
+
+    }
+
+    this.x += this.dx;
+    this.y += this.dy;
+    this.draw();
+  }
+}
+
+var saveMeDx = 2;
+var saveMeDy = 2;
+var saveMe = new Circle(200,200,2,2,20,'saveMe');
+var obstacleCircles = [];
+
+setInterval(addObstacleCircle, 5000);
+
 function animate() {
 
   requestAnimationFrame(animate);
-
   context.clearRect(0, 0, canvas.width, canvas.height);
-  context.beginPath();
-  context.arc(x, y, radius, 0, Math.PI * 2, false);
-  context.strokeStyle = 'blue';
-  context.stroke();
+  saveMe.update();
 
-  if(x + radius > canvas.width || x - radius < 0) {
-    dx = -dx;
+  for(looper = 0;looper < obstacleCircles.length; looper++) {
+    obstacleCircles[looper].update();
   }
 
-  if(y + radius > canvas.height || y - radius < 0) {
-    dy = -dy;
-  }
-
-  x += dx;
-  y += dy;
 }
+
+function addObstacleCircle() {
+
+  var obstacle = new Circle(20,20,2,2,20,'obstacle');
+  obstacleCircles.push(obstacle);
+}
+
+// arrow key detection
+document.onkeydown = function(e) {
+    switch (e.keyCode) {
+
+        case 37: //left
+
+            saveMe.dy = 0;
+            saveMe.dx = -saveMeDx;
+            break;
+
+        case 38: //up
+
+            saveMe.dx = 0;
+            saveMe.dy = -saveMeDy;
+            break;
+
+        case 39: //right
+
+            saveMe.dy = 0;
+            saveMe.dx = saveMeDx;
+            break;
+
+        case 40: //down
+
+            saveMe.dx = 0;
+            saveMe.dy = saveMeDy;
+            break;
+    }
+};
 
 animate();
